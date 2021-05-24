d05f99cf64a97e1b54670e7ff6371c3b0619962a
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-05-24 14:12:48 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee={
+    name: `Sam`,
+    streetAddress: `11 Broadway`,
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+const newEmployee = {...obj}
+newEmployee[key] = value
+return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value) {
+    obj[key] = value
+    return obj
+  }
+
+  function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj}
+    delete newEmployee[key]
+    return newEmployee
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(obj, key){
+      delete obj[key]
+      return obj
+  }
